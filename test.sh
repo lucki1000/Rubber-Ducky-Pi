e370785b6ceea90810af39d401758e8d2c935d69
Lukas Bonrath <ludwig.germersheim@gmail.com>
2019-11-16 11:51:21 +0100

    test to asynchron writing

---
+++ test.sh
@@ -0,0 +1,18 @@
+first_shasum="1"
+shasum="$(shasum payload.txt)"
+while true:
+	sleep 5
+	shasum="$(shasum payload.txt)"
+	if[ first_shasum =! shasum  ]
+	then
+		/home/pi/duckpi.sh de payload.txt
+		first_shasum="$(shasum payload.txt)"
+		echo "Execute_11"
+	fi
+	if[ first_shasum == shasum ]
+	then
+		first_shasum="$(shasum payload.txt)"
+		echo "Execute_00"
+	fi
+	echo "EXEC."
+done
