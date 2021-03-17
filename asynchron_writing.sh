b504c0eb33a7b3669e98b98b1b9ec4accfa55c4a
Lukas <51073652+lucki1000@users.noreply.github.com>
2021-03-17 20:52:06 +0100

    cleaning semantic error
    now it should work

--- asynchron_writing.sh
+++ asynchron_writing.sh
@@ -6,13 +6,13 @@ do
   shasum="$(shasum payload.txt)"
   if [ "$first_shasum" != "$shasum" ]
   then
-    /home/pi/duckpi.sh {$1} payload.txt
-    first_shasum="$(shasum payload.txt)"
+    /home/pi/duckpi.sh {$1} /home/pi/payload.txt
     echo "Execute_11"
   fi
   if [ "$first_shasum" == "$shasum" ]
   then
-    first_shasum="$(shasum payload.txt)"
     echo "Execute_00"
   fi
+  first_shasum="$(shasum payload.txt)"
+  sleep 3
 done
