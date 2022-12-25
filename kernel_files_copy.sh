1bf989e0e8fdac4273d5428c1bb9d3ef5767a166
lucki1000 <ludwig.germersheim@gmail.com>
2022-12-25 18:01:02 +0100

    Code cleanup & path correcting

--- kernel_files_copy.sh
+++ kernel_files_copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##Make all nessisary files executeable
 kernel="$(uname -r)"
-source /opt/Raspberry-Rubber-Ducky-Pi/.vars
+source /opt/Rubber-Ducky-Pi/.vars
 cd "$work_dir"
 chmod 755 hid-gadget-test.c duckpi.sh usleep.c g_hid.ko usleep hid-gadget-test hid-gadget-test_german.c
 
