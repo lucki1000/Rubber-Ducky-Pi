9356232d0927a0d2e0e4b7ef3a23705f23231f71
lucki1000 <ludwig.germersheim@gmail.com>
2022-12-25 18:31:05 +0100

    path correcting

--- kernel_files_copy.sh
+++ kernel_files_copy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ##Make all nessisary files executeable
 kernel="$(uname -r)"
-source /opt/Rubber-Ducky-Pi/.vars
+source "/opt/Rubber-Ducky-Pi/.vars"
 cd "$work_dir"
-chmod 755 hid-gadget-test.c duckpi.sh usleep.c g_hid.ko usleep hid-gadget-test hid-gadget-test_german.c
+chmod 755 duckpi.sh  g_hid.ko usleep hid-gadget-test
 
 \cp -r g_hid.ko /lib/modules/${kernel}/kernel/drivers/usb/gadget/legacy
 
