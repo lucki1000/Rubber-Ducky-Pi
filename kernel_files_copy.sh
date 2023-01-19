475c549f315cb4a0d721f557c8bba9dbc61d786a
Lukas Bonrath <ludwig.germersheim@gmail.com>
2023-01-19 18:13:34 +0100

    Revert "remove unecessary file & code"
    This reverts commit 2f5a3a69a2fa29ab63e61671a9291edb5c1ba491.

--- kernel_files_copy.sh
+++ kernel_files_copy.sh
@@ -4,3 +4,10 @@ kernel="$(uname -r)"
 source "/opt/Rubber-Ducky-Pi/.vars"
 cd "$work_dir"
 chmod 755 duckpi.sh usleep hid-gadget-test
+
+if [ "$1" != "hello" ]
+then
+    echo "Succses, your Raspberry Pi reboots in 5 seconds"
+    sleep 10
+    sudo reboot
+fi
